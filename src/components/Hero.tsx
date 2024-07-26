f63694720db7cfb1fba4efbcba47d5edbf125267
Mr.Bhanu <bhanudova02@gmail.com>
2024-07-26 17:23:27 +0530

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -29,7 +29,7 @@ const Hero: React.FC<HeroProps> = ({ heading, message }) => {
           <div className="absolute top-0 left-0 right-0 bottom-0  h-[40rem] md:h-[46rem] lg:h-[40rem] bg-black/70 z-[2]" />
           <div className="text-white z-[2] mt-0 lg:mt-10 px-2 md:px-0">
             <h3>Hello, I&apos;m</h3>
-            <h2 className="text-3xl md:text-5xl font-bold">{heading}</h2>
+            <h2 className="text-2xl md:text-5xl font-bold">{heading}</h2>
             <p className="py-2 md:py-5 text-xl leading-7 md:leading-10 lg:leading-10">
               As a front-end developer, I design captivating and responsive websites using HTML, CSS, Bootstrap, Tailwind CSS, JavaScript, TypeScript, ReactJS, and Next.js. Explore my projects to experience my dedication to creating intuitive and user-friendly web experiences.
             </p>
